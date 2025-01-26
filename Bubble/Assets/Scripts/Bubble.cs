2cf458c10f4969862917d061ca2066ebcbdb891e
Ricardo Bustamante <ricardo@busta.dev>
2025-01-26 15:15:26 +0100

    Add sound, fix particles and pop animation

--- Bubble/Assets/Scripts/Bubble.cs
+++ Bubble/Assets/Scripts/Bubble.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace DefaultNamespace
@@ -8,7 +9,27 @@ namespace DefaultNamespace
         public bool released;
 
         public bool popped;
-        
+
+        public static Stack<Bubble> bubblePool = new();
+        public static Stack<ParticleSystem> particlePool = new();
+
+        private float maxHeight;
+
+        public ParticleSystem particleSystemPrefab;
+
+        public static Bubble GetBubble(Bubble prefab, Vector2 position)
+        {
+            var bubble = bubblePool.Count == 0
+                ? Instantiate(prefab, position, Quaternion.identity)
+                : bubblePool.Pop();
+            bubble.popped = false;
+            bubble.released = false;
+            bubble.maxHeight = Mathf.Min(position.y + Random.Range(2f, 5f), 7f);
+            bubble.transform.position = position;
+            bubble.gameObject.SetActive(true);
+            return bubble;
+        }
+
         private void Awake()
         {
             released = false;
@@ -18,17 +39,33 @@ namespace DefaultNamespace
         {
             if (popped)
             {
-                
-            }else if (released)
+                StartCoroutine(PopBubble());
+            }
+            else if (released)
             {
                 transform.position += Vector3.up * Time.deltaTime;
 
-                if (transform.position.y > 5)
+                if (transform.position.y > maxHeight)
                 {
                     popped = true;
-                    gameObject.SetActive(false);
                 }
             }
+        }
+
+        public IEnumerator PopBubble()
+        {
+            gameObject.SetActive(false);
+            bubblePool.Push(this);
+
+            var particle = particlePool.Count > 0
+                ? particlePool.Pop()
+                : Instantiate(particleSystemPrefab, transform.position, Quaternion.identity);
+            particle.Play();
+
+            yield return new WaitForSeconds(particle.main.duration);
+            
+            particle.gameObject.SetActive(false);
+            particlePool.Push(particle);
         }
     }
 }
--- Bubble/Assets/Scripts/InputController.cs
+++ Bubble/Assets/Scripts/InputController.cs
@@ -1,31 +1,52 @@
 using System;
+using System.Collections.Generic;
 using DefaultNamespace;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.InputSystem;
 
 public class InputController : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
     public Camera camera;
     public Bubble bubblePrefab;
 
+    private Stack<Bubble> bubblePool;
+
+    public AudioSource audioSource;
+
     private Bubble bubble;
 
     private bool blowing;
     private float scale;
-    
+
     public void OnPointerDown(PointerEventData eventData)
     {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
         var ray = camera.ScreenPointToRay(Input.mousePosition);
-        bubble = Instantiate(bubblePrefab, camera.transform.position + ray.direction * 10, Quaternion.identity);
+        var pos = camera.transform.position + ray.direction * 10;
+        bubble = Bubble.GetBubble(bubblePrefab, pos);
         blowing = true;
+        audioSource.volume = 1;
+        audioSource.Play();
         scale = 0.01f;
     }
 
     public void OnPointerUp(PointerEventData eventData)
     {
+        if (eventData.button != PointerEventData.InputButton.Left)
+        {
+            return;
+        }
+
         bubble.released = true;
         bubble = null;
         blowing = false;
+        audioSource.volume = 0;
+        audioSource.Stop();
     }
 
     private void Update()
@@ -34,6 +55,16 @@ public class InputController : MonoBehaviour, IPointerDownHandler, IPointerUpHan
         {
             scale += 1 * Time.deltaTime;
             bubble.transform.localScale = new Vector3(scale, scale, scale);
+
+            audioSource.pitch = Mathf.Pow(scale / 5f, 0.5f);
+
+            if (scale > 5)
+            {
+                bubble.popped = true;
+                blowing = false;
+                audioSource.volume = 0;
+                audioSource.Stop();
+            }
         }
     }
 }
